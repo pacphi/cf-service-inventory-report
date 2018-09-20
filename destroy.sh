e030210e399a87d2fc480eb31209be572962371f
Chris Phillipson <cphillipson@pivotal.io>
2018-09-20 16:19:35 -0500

    Provide two methods for externalizing secrets * Allow for deployment with either a credhub or user-provided service instance * Update dependencies to be managed be Spring Boot Parent 2.0.5.RELEASE * Update documentation and scripts to support both methods of deployment * Add a manifest.yml * Add a secrets profile to application.yml; select properties may be overridden from default configuration * Default back-end provider is jdbc

--- destroy.sh
+++ destroy.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-cf stop get-service-inventory-task
-cf delete get-service-inventory-task
+export APP_NAME=cf-service-inventory-report
+
+cf stop $APP_NAME
+cf unbind-service $APP_NAME $APP_NAME-secrets
+cf delete-service $APP_NAME-secrets -f
+cf delete $APP_NAME -r -f
