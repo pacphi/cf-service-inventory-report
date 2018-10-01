9e135ac334b9d4daa93cc36c4c9220b8ebc0af53
Chris Phillipson <cphillipson@pivotal.io>
2018-10-01 14:17:45 -0700

    Update destroy.sh script

--- destroy.sh
+++ destroy.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
+set -x
 
 export APP_NAME=cf-service-inventory-report
 
-cf stop $APP_NAME
-cf unbind-service $APP_NAME $APP_NAME-secrets
-cf delete-service $APP_NAME-secrets -f
-cf delete $APP_NAME -r -f
+cf app ${APP_NAME} --guid
+
+if [ $? -eq 0 ]; then
+	cf stop $APP_NAME
+	cf unbind-service $APP_NAME $APP_NAME-secrets
+	cf delete-service $APP_NAME-secrets -f
+	cf delete $APP_NAME -r -f
+else
+    echo "$APP_NMAE does not exist"
+fi
